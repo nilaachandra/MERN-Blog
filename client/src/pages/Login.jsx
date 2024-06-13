cdca4a733f27fcda77e59441a25b05fcd97e32f4
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-06-13 14:34:36 +0530

    minor css changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -67,14 +67,14 @@ const Login = () => {
           {!revealPassword && (
             <IoEyeOutline
               onClick={() => setRevealPassword(!revealPassword)}
-              className="absolute right-4 cursor-pointer top-[58%]"
+              className="absolute right-4 cursor-pointer top-[57%]"
               size={24}
             />
           )}
           {revealPassword && (
             <IoEyeOffOutline
               onClick={() => setRevealPassword(!revealPassword)}
-              className="absolute right-4 cursor-pointer top-[58%]"
+              className="absolute right-4 cursor-pointer top-[57%]"
               size={24}
             />
           )}
